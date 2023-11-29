e418a07aaed1d2c434d6c4cc742b0c530e619988
wsdoprado <79108556+wsdoprado@users.noreply.github.com>
2023-11-28 21:22:05 -0300

    Create nornir2.py

---
+++ nornir2.py
@@ -0,0 +1,18 @@
+"""
+Script: Show Interfaces Brief - Nornir + Netmiko + Filter
+Author: William Prado
+Email: wprado@nic.br
+"""
+
+from nornir import InitNornir
+from nornir_utils.plugins.functions import print_result
+from nornir_netmiko.tasks.netmiko_send_command import netmiko_send_command
+
+nr = InitNornir(config_file="hosts.yaml")
+
+r4 = nr.filter(name="R4")
+
+results = r4.run(netmiko_send_command, command_string="show interfaces brief")
+
+print_result(results)
+
