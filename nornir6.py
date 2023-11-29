7f65b250f2bd5586feb7961b871a714600eaf7b3
wsdoprado <79108556+wsdoprado@users.noreply.github.com>
2023-11-28 21:30:00 -0300

    Create nornir6.py

---
+++ nornir6.py
@@ -0,0 +1,36 @@
+"""
+Script: Change Hostname - Nornir + Napalm + Filter + Netbox 
+Author: William Prado
+Email: wprado@nic.br
+"""
+
+from nornir import InitNornir
+from nornir_utils.plugins.functions import print_result
+from nornir_netmiko import netmiko_send_config, netmiko_commit
+import os
+
+def nornir_netmiko_send_config(task):
+    configurations = ["hostname "+str(task.host.name)]
+    command = task.run(netmiko_send_config, config_commands=configurations)
+    print_result(command)
+    commit = task.run(netmiko_commit)
+    print_result(commit)
+
+nr = InitNornir(
+    inventory={
+         "plugin": "NetBoxInventory2",
+          "options": {
+             "nb_url": os.getenv("NETBOX_URL"),
+             "nb_token": os.getenv("NETBOX_TOKEN"),
+             "filter_parameters": {"tenant": ["production"], 
+                                   "role": ["l2","pe","p"], 
+                                   "region": ["sp","pr","rj","mg"], 
+                                   "status": "active", 
+                                   "platform": ["iosxr"]},
+             "ssl_verify": False}})
+
+nr.inventory.defaults.username = os.getenv("USER")
+nr.inventory.defaults.password = os.getenv("PASSWORD")
+nr.inventory.defaults.port = os.getenv("SSH_PORT")
+
+nr.run(task=nornir_netmiko_send_config)
