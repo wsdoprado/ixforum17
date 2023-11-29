ae93ab87d2885647d81e5aa7562d17622c3e12ca
wsdoprado <79108556+wsdoprado@users.noreply.github.com>
2023-11-28 21:24:52 -0300

    Create nornir4.py

---
+++ nornir4.py
@@ -0,0 +1,31 @@
+"""
+Script: Get Interface - Nornir + Napalm + Filter + Netbox
+Author: William Prado
+Email: wprado@nic.br
+"""
+
+from nornir import InitNornir
+from nornir_napalm.plugins.tasks import napalm_get
+from nornir_rich.functions import print_result
+import os
+
+nr = InitNornir(
+        inventory={
+            "plugin": "NetBoxInventory2",
+            "options": {
+                "nb_url": os.getenv("NETBOX_URL"),
+                "nb_token": os.getenv("NETBOX_TOKEN"),
+                "filter_parameters": {
+                    "tenant": "production", 
+                    "role": "l2", 
+                    "region": "mg", 
+                    "status": "active", 
+                    "platform": "iosxr"},
+                "ssl_verify": False}
+        })
+
+nr.inventory.defaults.username = os.getenv("USER")
+nr.inventory.defaults.password = os.getenv("PASSWORD")
+nr.inventory.defaults.port = os.getenv("SSH_PORT")
+
+print_result(nr.run(task=napalm_get, getters=["get_interfaces"]), vars=["result", "name"])
