b52fc61c7a262d4991a71856ae9c488ab6e57d30
wsdoprado <79108556+wsdoprado@users.noreply.github.com>
2023-11-28 21:23:30 -0300

    Create nornir3.py

---
+++ nornir3.py
@@ -0,0 +1,15 @@
+"""
+Script: Get Interfaces - Nornir + Napalm + Filter
+Author: William Prado
+Email: wprado@nic.br
+"""
+
+from nornir import InitNornir
+from nornir_napalm.plugins.tasks import napalm_get
+from nornir_rich.functions import print_result
+
+nr = InitNornir(config_file="hosts.yaml")
+
+r2 = nr.filter(name="R2")
+
+print_result(r2.run(task=napalm_get, getters=["get_interfaces"]), vars=["result"])
