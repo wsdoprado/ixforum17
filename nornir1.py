49c73cefe27ea6a7556c70b6a3cc4b74cd7dd5d6
wsdoprado <79108556+wsdoprado@users.noreply.github.com>
2023-12-01 13:03:40 -0300

    Update nornir1.py

--- nornir1.py
+++ nornir1.py
@@ -9,7 +9,9 @@ from nornir_rich.functions import print_result
 from nornir_netmiko.tasks.netmiko_send_command import netmiko_send_command
 
 
-nr = InitNornir(config_file="hosts.yaml")
+nr = InitNornir(
+  runner={"plugin": "threaded", "options": {"num_workers": 50}},
+  config_file="hosts.yaml")
 
 
 print_result(nr.run(netmiko_send_command, command_string="show interfaces brief"))
