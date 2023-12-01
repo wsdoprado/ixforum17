5433afe39f37b2e12b634530de034abf9a43968b
wsdoprado <79108556+wsdoprado@users.noreply.github.com>
2023-12-01 13:04:06 -0300

    Update nornir2.py

--- nornir2.py
+++ nornir2.py
@@ -8,7 +8,9 @@ from nornir import InitNornir
 from nornir_utils.plugins.functions import print_result
 from nornir_netmiko.tasks.netmiko_send_command import netmiko_send_command
 
-nr = InitNornir(config_file="hosts.yaml")
+nr = InitNornir(
+  runner={"plugin": "threaded", "options": {"num_workers": 20}},
+  config_file="hosts.yaml")
 
 r4 = nr.filter(name="R4")
 
